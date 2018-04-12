474952a3a1411e2a10396e77c147302a40089389
Anshul Jain <anshul2896@gmail.com>
2018-04-12 20:22:56 +0530

    model code (1/2)

--- code/model.py
+++ code/model.py
@@ -3,7 +3,10 @@ import torch.nn as nn
 from miscc.config import cfg
 from torchvision import models
 import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+from torch.autograd import Variable
 
+from GlobalAttention import GlobalAttentionGeneral as ATT_NET
 
 # ############## Text2Image Encoder-Decoder #######
 class RNN_ENCODER(nn.Module):
@@ -112,3 +115,180 @@ class CNN_ENCODER(nn.Module):
         initrange = 0.1
         self.emb_features.weight.data.uniform(-initrange, initrange)
         self.emb_cnn_code.weight.data.uniform(-initrange, initrange)
+
+class GLU(nn.Module):           ######## purpose ???
+    def __init__(self):
+        super(GLU, self).__init__()
+
+    def forward(self, x):
+        nc = x.size(1)
+        assert nc%2==0, 'channels not divisible by 2.'
+        nc = int(nc/2)
+        return x[:, :nc]*F.sigmoid(x[:, nc:])
+
+def conv3x3(in_planes, out_planes):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1,
+                     padding=1, bias=False)
+
+# upscale spatial size by 2
+def upBlock(in_planes, out_planes):
+    block = nn.Sequential(
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        conv3x3(in_planes, out_planes*2),
+        nn.BatchNorm2d(out_planes*2),
+        GLU()
+    )
+    return block
+
+class ResBlock(nn.Module):
+    def __init__(self, channel_num):
+        super(ResBlock, self).__init__()
+        self.block = nn.Sequential(
+            conv3x3(channel_num, channel_num*2),
+            nn.BatchNorm2d(channel_num*2),
+            GLU(),
+            conv3x3(channel_num, channel_num),
+            nn.BatchNorm2d(channel_num)
+        )
+
+    def forward(self, x):
+        residual = x
+        out = self.block(x)
+        out += residual
+        return out
+
+###### Generator Code ####################
+######Conditioning augmentation####
+class CA_NET(nn.Module):
+    def __init__(self):
+        super(CA_NET, self).__init__()
+        self.t_dim = cfg.TEXT.EMBEDDING_DIM
+        self.c_dim = cfg.GAN.CONDITION_DIM
+        self.fc = nn.Linear(self.t_dim, self.c_dim*4, bias=True)
+        self.relu = GLU()
+
+    def encode(self, text_embedding):
+        x = self.relu(self.fc(text_embedding))
+        mu = x[:, :self.c_dim]
+        logvar = x[:, self.c_dim:]
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        if cfg.CUDA:
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, text_embedding):
+        mu, logvar =self.encode(text_embedding)
+        c_code = self.reparameterize(mu, logvar)
+        return c_code, mu, logvar
+
+
+class INIT_STAGE_G(nn.Module):
+    def __init__(self, ngf, ncf):
+        super(INIT_STAGE_G, self).__init__()
+        self.gf_dim = ngf
+        self.in_dim = cfg.GAN.Z_DIM + ncf  # total input dim = noise dim + CA dimension
+
+        self.define_module()
+
+    def define_module(self):
+        nz, ngf = self.in_dim, self.gf_dim
+        self.fc = nn.Sequential(
+            nn.Linear(nz, ngf*4*4*2, bias=False),
+            nn.BatchNorm1d(ngf*4*4*2),
+            GLU()
+        )
+        self.upsample1 = upBlock(ngf, ngf//2)
+        self.upsample2 = upBlock(ngf//2, ngf//4)
+        self.upsample3 = upBlock(ngf//4, ngf//8)
+        self.upsample4 = upBlock(ngf//8, ngf//16)
+
+    def forward(self, z_code, c_code):
+        """
+        :param z_code: batch x cfg.GAN.Z_DIM
+        :param c_code: batch x cfg.TEXT.EMBEDDING_DIM
+        :return: batch x ngf/16 x64 x 64
+        """
+        c_z_code = torch.cat((c_code, z_code), 1)
+        # state size ngf * 4 * 4
+        out_code = self.fc(c_z_code)
+        out_code = out_code.view(-1, self.gf_dim, 4, 4)
+        # state size ngf/3 * 8 * 8
+        out_code = self.upsample1(out_code)
+        # state size ngf/4 * 16 * 16
+        out_code = self.upsample2(out_code)
+        # state size ngf/8 * 32 * 32
+        out_code32 = self.upsample3(out_code)
+        # state suze ngf/16 * 64 * 64
+        out_code64 = self.upsample4(out_code32)
+
+        return out_code64
+
+class GET_IMAGE_G(nn.Module):
+    def __init__(self, ngf):
+        super(GET_IMAGE_G, self).__init__()
+        self.gf_dim = ngf
+        self.img = nn.Sequential(
+            conv3x3(ngf, 3),
+            nn.Tanh()
+        )
+
+    def forward(self, h_code):
+        out_img = self.img(h_code)
+        return out_img
+
+class NEXT_STAGE_G(nn.Module):
+    def __init__(self, ngf, nef, ncf):
+        super(NEXT_STAGE_G, self).__init__()
+        self.gf_dim = ngf
+        self.ef_dim = nef
+        self.cf_dim = ncf
+        self.num_residual = cfg.GAN.R_NUM  ### ????? what is R_NUM
+        self.define_module()
+
+    def make_layer(self, block, channel_num):
+        layers = []
+        for i in range(cfg.GAN.R_NUM):
+            layers.append(block(channel_num))
+
+        return nn.Sequential(*layers)
+
+    def define_module(self):
+        ngf = self.gf_dim
+        self.att = ATT_NET(ngf, self.ef_dim)
+        self.residual = self.make_layer(ResBlock, ngf * 2)
+        self.upsample = upBlock(ngf * 2, ngf)
+
+    def forward(self, h_code, c_code, word_embs, mask):
+        self.att.applyMask(mask)
+        c_code, att = self.att(h_code, word_embs)
+        h_c_code = torch.cat((h_code, c_code), 1)
+        out_code =  self.residual(h_c_code)
+
+        #state size ngf/2 * 2in_size * 2in_size
+        out_code = self.upsample(out_code)
+        return out_code, att
+
+class G_NET(nn.Module):
+    def __init__(self):
+        super(G_NET, self).__init__()
+        ngf = cfg.GAN.GF_DIM
+        nef = cfg.TEXT.EMBEDDING_DIM
+        ncf = cfg.GAN.CONDITION_DIM
+        self.ca_net = CA_NET()
+
+        if cfg.TREE.BRANCH_NUM > 0:
+            self.h_net1 = INIT_STAGE_G(ngf * 16, ncf)
+            self.img_net1 = GET_IMAGE_G(ngf)
+        if cfg.TREE.BRANCH_NUM > 1:
+            self.h_net2 = NEXT_STAGE_G(ngf, nef, ncf)
+            self.img_net2 = GET_IMAGE_G(ngf)
+        if cfg.TREE.BRANCH_NUM > 2:
+            self.h_net3 = NEXT_STAGE_G(ngf, nef, ncf)
+            self.img_net3 = GET_IMAGE_G(ngf)
